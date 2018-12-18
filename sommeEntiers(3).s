bdb14f278098544f697e534425cf81aa1fad83ca
Theo <theoa.dragan@yahoo.com>
2018-12-18 18:47:38 +0100

    Avec lire n de stdin.

--- sommeEntiers(3).s
+++ sommeEntiers(3).s
@@ -1,5 +1,8 @@
 .data
 n: .word 10
+enter:	.asciz "Entrer n : "
+scan:	.asciz "%d"
+return: .word	0
 msg: .asciz "La somme des entiers est : %d \n"
 .text
 .global main
@@ -28,14 +31,23 @@ endsum:
  bx lr
 
 sum0:
- mov r0, #0 // optional, we already had 0 in r0, but just to respect the pattern. and maybe understand my work later on.
+ mov r0, #0 // optional, we already had 0 in r0, but just to respect the pattern.
  sub sp, sp, #8
  str r0, [sp] // storing the value 0 for sum(0)
  b endsum
 
 main:
- ldr r1, =n
- ldr r0, [r1]
+
+ // to read n from stdin and put it in r0
+ ldr r0, =enter
+ bl printf
+ ldr r0, =scan
+ ldr r1, =return
+ bl scanf
+ ldr r1, =return
+ ldr r2, [r1]
+ mov r0,r2
+
  bl sum
  mov r1, r0
  ldr r0, =msg
