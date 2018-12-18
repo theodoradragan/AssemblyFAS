8ba4809118b27ea8ce72c9e05040a3a0d3f7286a
Theo <theoa.dragan@yahoo.com>
2018-12-18 17:58:04 +0100

    Ca marche !!

--- permuterValeurs(1).s
+++ permuterValeurs(1).s
@@ -1,14 +1,15 @@
 .data
 x: .word 10
 y: .word 20
+msg: .asciz "x est : %d et y est : %d \n"
 
 .text
 .global main
 
 swap:
- mov r3, =x		// Put address of x in r3
+ ldr r3, =x		// Put address of x in r3
  ldr r1, [r3]	// Put value of r3 in r1
- mov r4, =y		// Put address of y in r4
+ ldr r4, =y		// Put address of y in r4
  ldr r2, [r4]	// Put value of r4 in r2
  sub sp, sp, #8	// Alloc 8 bytes on stack
  str r1, [sp]	// Put value of r1 in stack
@@ -22,4 +23,14 @@ swap:
 
 main:
  bl swap
+
+ ldr r0, =msg
+
+ ldr r3, =x
+ ldr r1, [r3]
+
+ ldr r4, =y
+ ldr r2, [r4]
+
+ bl printf
  bl exit
